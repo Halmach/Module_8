5943c2fdb9ebb9743f65bed6577f3e3c24f39ffa
Khalmatov <large000@yandex.ru>
2021-11-03 22:50:10 +0300

    Задание 8.1.4

--- Program.cs
+++ Program.cs
@@ -9,4 +9,25 @@ namespace Module_8
             Console.WriteLine("Hello World!");
         }
     }
+
+    class Disk
+    {
+        private string diskName;
+        private int diskSpace;
+        private int freeSpace;
+
+
+        public string DiskName => diskName;
+
+        public int DiskSize => diskSpace;
+
+        public int FreeSize => freeSpace;
+
+        public Disk(string diskName,int diskSpace,int freeSize)
+        {
+            this.diskSpace = diskSpace;
+            this.diskName = diskName;
+            this.freeSpace = freeSize;
+        }
+    }
 }
