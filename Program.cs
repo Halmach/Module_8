0ceafdab98d6ccfdba6300625680ef7fa7bd86df
Khalmatov <large000@yandex.ru>
2021-11-05 23:09:05 +0300

    Задание 8.3.1

--- Program.cs
+++ Program.cs
@@ -4,86 +4,145 @@ using System.IO;
 
 namespace Module_8
 {
+
+
     class Program
     {
         static void Main(string[] args)
-        {/*
-            DriveInfo[] drives = DriveInfo.GetDrives();
-
-            foreach (DriveInfo drive in drives)
-            {
-                Console.WriteLine($"Название: {drive.Name}");
-                Console.WriteLine($"Тип: {drive.DriveType}");
-                if (drive.IsReady)
-                {
-                    Console.WriteLine($"Объем:{drive.TotalSize}");
-                    Console.WriteLine($"Свободно:{drive.TotalFreeSpace}");
-                    Console.WriteLine($"Метка:{drive.VolumeLabel}");
-
-                }
-            }*/
-
-          //  GetCatalogs();
-          string dirName = @"C:\SomeDir";
-          string subDirName = @"Splinter Cell\Double_Agent";
-
-          try
-          {
-                Console.WriteLine("Папка не создана");
-                DirectoryInfo newDirectory = new DirectoryInfo(dirName); 
-                if(!newDirectory.Exists) newDirectory.Create();
-                else Console.WriteLine(newDirectory.GetDirectories().Length + newDirectory.GetFiles().Length);
-                Console.WriteLine("Папка  создана");
-                Console.WriteLine(newDirectory.GetDirectories().Length + newDirectory.GetFiles().Length);
-                Console.WriteLine("Создана вложенная папка");
-                newDirectory.CreateSubdirectory(subDirName);
-                Console.WriteLine(newDirectory.GetDirectories().Length + newDirectory.GetFiles().Length);
-
-
-
-                Console.WriteLine($"Название каталога: {newDirectory.Name}");
-                Console.WriteLine($"Полное название каталога: {newDirectory.FullName}");
-                Console.WriteLine($"Время создания каталога: {newDirectory.CreationTime}");
-                Console.WriteLine($"Корневой каталог: {newDirectory.Root}");
-
-
-                newDirectory.Delete(true);
-                Console.WriteLine("Каталог удален");
-
-            }
-          catch (Exception e)
-          {
-                Console.WriteLine(e.Message);
-          }
-
-        }
-
-        static void GetCatalogs()
         {
-            int fileCnt = 0;
-            int directoryCnt = 0;
-            string dirName = @"C://";
-            if (Directory.Exists(dirName))
+
+            string filePath = @"C:\Users\pc\source\dev\Module_8\Module_8\Program.cs";
+            if (File.Exists(filePath))
             {
-             //   Console.WriteLine("Папки:");
-                string[] dirs = Directory.GetDirectories(dirName);
-                foreach (string d in dirs)
+                using (StreamReader sr = File.OpenText(filePath))
                 {
-                    // Console.WriteLine(d);
-                    fileCnt++;
-                }
-          //      Console.WriteLine("Файлы:");
-                string[] files = Directory.GetFiles(dirName);
-                foreach (string s in files)
-                {
-                    //  Console.WriteLine(s);
-                    directoryCnt++;
+                    string str = "";
+                    while ((str = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(str);
+                    }
                 }
             }
-            Console.WriteLine("File num: " + fileCnt);
-            Console.WriteLine("Directory num: " + directoryCnt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+          //  DriveInfo[] drives = DriveInfo.GetDrives();
+
+          //  foreach (DriveInfo drive in drives)
+          //  {
+          //      Console.WriteLine($"Название: {drive.Name}");
+          //      Console.WriteLine($"Тип: {drive.DriveType}");
+          //      if (drive.IsReady)
+          //      {
+          //          Console.WriteLine($"Объем:{drive.TotalSize}");
+          //          Console.WriteLine($"Свободно:{drive.TotalFreeSpace}");
+          //          Console.WriteLine($"Метка:{drive.VolumeLabel}");
+
+          //      }
+          //  }*/
+
+            ////  GetCatalogs();
+            //string dirName = @"C:\testFolder";
+            //string subDirName = @"Splinter Cell\Double_Agent";
+
+            //try
+            //{
+            //      Console.WriteLine("Папка не создана");
+            //      DirectoryInfo newDirectory = new DirectoryInfo(dirName); 
+            //      if(!newDirectory.Exists) newDirectory.Create();
+
+            //      newDirectory.MoveTo(@"C:\$Recycle.Bin\S-1-5-21-2602286027-2564388254-2951404948-1001\testFloder");
+            //      //else Console.WriteLine(newDirectory.GetDirectories().Length + newDirectory.GetFiles().Length);
+            //      //Console.WriteLine("Папка  создана");
+            //      //Console.WriteLine(newDirectory.GetDirectories().Length + newDirectory.GetFiles().Length);
+            //      //Console.WriteLine("Создана вложенная папка");
+            //      //newDirectory.CreateSubdirectory(subDirName);
+            //      //Console.WriteLine(newDirectory.GetDirectories().Length + newDirectory.GetFiles().Length);
+
+
+
+            //      //Console.WriteLine($"Название каталога: {newDirectory.Name}");
+            //      //Console.WriteLine($"Полное название каталога: {newDirectory.FullName}");
+            //      //Console.WriteLine($"Время создания каталога: {newDirectory.CreationTime}");
+            //      //Console.WriteLine($"Корневой каталог: {newDirectory.Root}");
+
+
+            //    //  newDirectory.Delete(true);
+            //    //  Console.WriteLine("Каталог удален");
+
+            //  }
+            //catch (Exception e)
+            //{
+            //      Console.WriteLine(e.Message);
+
+
         }
-    }
+
+        //static void GetCatalogs()
+        //{
+        //    int fileCnt = 0;
+        //    int directoryCnt = 0;
+        //    string dirName = @"C://";
+        //    if (Directory.Exists(dirName))
+        //    {
+        //     //   Console.WriteLine("Папки:");
+        //        string[] dirs = Directory.GetDirectories(dirName);
+        //        foreach (string d in dirs)
+        //        {
+        //            // Console.WriteLine(d);
+        //            fileCnt++;
+        //        }
+        //  //      Console.WriteLine("Файлы:");
+        //        string[] files = Directory.GetFiles(dirName);
+        //        foreach (string s in files)
+        //        {
+        //            //  Console.WriteLine(s);
+        //            directoryCnt++;
+        //        }
+        //    }
+        //    Console.WriteLine("File num: " + fileCnt);
+        //    Console.WriteLine("Directory num: " + directoryCnt);
+        }
+    
 
     class Disk
     {
